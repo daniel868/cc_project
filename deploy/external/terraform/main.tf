bec9c95451db1027a46d493e11437a4977a0236e
daniel868 <sincalexandrudaniel@gmail.com>
2024-01-07 16:43:43 +0200

    cluster config

--- deploy/external/terraform/main.tf
+++ deploy/external/terraform/main.tf
@@ -1,31 +1,29 @@
 terraform {
   required_providers {
-    minikube = {
-      source  = "scott-the-programmer/minikube"
-      version = "0.3.7"
+    kind = {
+      source = "tehcyx/kind"
+      version = "0.2.1"
     }
   }
 }
 
-provider "minikube" {
-  kubernetes_version = "v1.28.3"
+provider "kind" {}
+
+resource "kind_cluster" "default" {
+  name = "cc-cluster"
+  node_image = "kindest/node:v1.27.3"
+
+  kind_config {
+    api_version = "kind.x-k8s.io/v1alpha4"
+    kind        = "Cluster"
+    node {
+      role = "control-plane"
+    }
+    node {
+      role = "worker"
+    }
+  }
 }
 
-resource "minikube_cluster" "docker" {
-  driver       = "docker"
-  cluster_name = "terraform-provider-minikube-acc-docker"
-  addons = [
-    "default-storageclass",
-    "storage-provisioner"
-  ]
-}
-
-provider "kubernetes" {
-  host = minikube_cluster.docker.host
-
-  client_certificate     = minikube_cluster.docker.client_certificate
-  client_key             = minikube_cluster.docker.client_key
-  cluster_ca_certificate = minikube_cluster.docker.cluster_ca_certificate
-}
 
 
