3fbef8631c0f1bdd235cb946e3ae550e50937404
Cristiana959 <cristiana699@gmail.com>
2024-01-07 17:21:58 +0200

    db config files

---
+++ deploy/db/init.sql
@@ -0,0 +1,5 @@
+CREATE USER postgres;
+CREATE DATABASE postgres;
+GRANT ALL PRIVILEGES ON DATABASE postgres TO postgres;
+ALTER USER postgres with PASSWORD 'postgres';
+ALTER USER postgres with PASSWORD 'postgres';
