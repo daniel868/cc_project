d92bea2857466f4e8ed977d40fad8b87a508c79b
daniel868 <sincalexandrudaniel@gmail.com>
2024-01-13 15:34:48 +0200

    add fe deployment

---
+++ deploy/external/terraform/fe.tf
@@ -0,0 +1,54 @@
+resource "kubernetes_deployment" "fe_deployment" {
+  metadata {
+    name   = "fe"
+    labels = {
+      app = "fe"
+    }
+  }
+
+  spec {
+    replicas = 1
+
+    selector {
+      match_labels = {
+        app = "fe"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "fe"
+        }
+      }
+
+      spec {
+        container {
+          image = "alexandrudaniel/cc_fe:latest"
+          name  = "cc-fe"
+
+          port {
+            container_port = 4200
+          }
+
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_service" "fe_service" {
+  metadata {
+    name = "fe-service"
+  }
+  spec {
+    selector = {
+      app = "fe"
+    }
+    type = "NodePort"
+    port {
+      port        = 4200
+      target_port = 4200
+    }
+  }
+}
