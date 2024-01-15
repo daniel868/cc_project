3364c8b3d27f32ae1bc5c5a761ed75d962b82c1a
daniel868 <sincalexandrudaniel@gmail.com>
2024-01-15 20:25:53 +0200

    change image

--- deploy/external/terraform/business-logic.tf
+++ deploy/external/terraform/business-logic.tf
@@ -35,7 +35,7 @@ resource "kubernetes_deployment" "business_logic" {
       spec {
         container {
           name  = "business-logic"
-          image = "cristianaandrei/cc:business_logic2:latest"
+          image = "alexandrudaniel/business_logic:latest"
 
           port {
             container_port = 8080
