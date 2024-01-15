2b50ba923f5bbb30be5b595179a7861b69e1b079
daniel868 <sincalexandrudaniel@gmail.com>
2024-01-16 01:57:16 +0200

    change urls

--- cc_fe/cc_fe/src/environments/environment.prod.ts
+++ cc_fe/cc_fe/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   business_base_url: 'http://business-logic-service:8082',
-  register_url: 'http://20.150.198.103:8081/api/auth/signup',
-  login_url:'http://20.150.198.103:8081/api/auth/login'
+  register_url: 'http://auth-service:8081/api/auth/signup',
+  login_url:'http://auth-service:8081/api/auth/login'
 };
