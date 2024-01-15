d54dd47a661eb93ead7a62003e705b6b9698fd3b
daniel868 <sincalexandrudaniel@gmail.com>
2024-01-16 01:12:59 +0200

    add reservation component

--- cc_fe/cc_fe/src/app/app.module.ts
+++ cc_fe/cc_fe/src/app/app.module.ts
@@ -14,6 +14,7 @@ import {HeaderComponent} from './header/header.component';
 import {DropdownDirective} from "./dropdown.directive";
 import {HttpClient, HttpClientModule} from "@angular/common/http";
 import { LoginComponent } from './login/login.component';
+import { ReservationComponent } from './main/reservation/reservation.component';
 
 @NgModule({
   declarations: [
@@ -25,7 +26,8 @@ import { LoginComponent } from './login/login.component';
     RestaurantComponent,
     HeaderComponent,
     DropdownDirective,
-    LoginComponent
+    LoginComponent,
+    ReservationComponent
   ],
   imports: [
     BrowserModule,
