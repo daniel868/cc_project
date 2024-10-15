f492d94f1b34eac703279beb16e82d69bcbce1fe
daniel868 <sincalexandrudaniel@gmail.com>
2024-10-15 21:09:01 +0300

    init mock data

--- cc_backed/business/src/main/java/org/business/BusinessLogicApplication.java
+++ cc_backed/business/src/main/java/org/business/BusinessLogicApplication.java
@@ -1,11 +1,28 @@
 package org.business;
 
+import org.business.model.Restaurant;
+import org.business.repository.RestaurantRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BusinessLogicApplication {
     public static void main(String[] args) {
         SpringApplication.run(BusinessLogicApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner runner(RestaurantRepository restaurantRepository) {
+        return args -> {
+            Restaurant restaurant = new Restaurant();
+            restaurant.setAddress("test");
+            restaurant.setAvailableSpots(10);
+            restaurant.setImageUrl("https://cdn.vox-cdn.com/thumbor/qVw98okiGmZ4-v2EypqK7ObXMKc=/0x0:6240x4160/1200x900/filters:focal(2621x1581:3619x2579):no_upscale()/cdn.vox-cdn.com/uploads/chorus_image/image/62582192/IMG_2025.280.jpg");
+            restaurant.setName("test");
+            restaurant.setOwner("test");
+            restaurantRepository.save(restaurant);
+        };
     }
 }
