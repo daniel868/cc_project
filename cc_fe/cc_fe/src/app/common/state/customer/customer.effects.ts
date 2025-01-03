c8ed9585fd3ebe449aef06940d0ce6ae390222f3
daniel868 <sincalexandrudaniel@gmail.com>
2025-01-03 23:27:40 +0200

    updating customer & users table

--- cc_fe/cc_fe/src/app/common/state/customer/customer.effects.ts
+++ cc_fe/cc_fe/src/app/common/state/customer/customer.effects.ts
@@ -2,12 +2,12 @@ import {inject, Injectable} from "@angular/core";
 import {Actions, createEffect, ofType} from "@ngrx/effects";
 import {HttpClient, HttpParams} from "@angular/common/http";
 import {FinishFetchReservationAction, StartFetchReservationAction} from "../reservation/reservation.actions";
-import {catchError, map, of, switchMap} from "rxjs";
+import {catchError, exhaustMap, map, of, switchMap} from "rxjs";
 import {PageableGenericResponse} from "../../shared/pageable-generic-response";
 import {Reservation} from "../../../model/reservation";
 import {environment} from "../../../../environments/environment";
-import {GenericFailedAction} from "../shared/shared.actions";
-import {FinishFetchCurrentCustomer, StartFetchCurrentCustomer} from "./customer.actions";
+import {GenericFailedAction, GenericSuccessAction} from "../shared/shared.actions";
+import {FinishFetchCurrentCustomer, StartFetchCurrentCustomer, UpdateCustomerAction} from "./customer.actions";
 import {Customer} from "../../../model/customer";
 
 @Injectable()
@@ -17,7 +17,7 @@ export class CustomerEffects {
   constructor(private httpClient: HttpClient) {
   }
 
-  fetchCurrentCustomer = createEffect(() =>
+  fetchCurrentCustomerEffect = createEffect(() =>
     this.actions$.pipe(
       ofType(StartFetchCurrentCustomer),
       switchMap(props => {
@@ -28,6 +28,29 @@ export class CustomerEffects {
             }),
             catchError(err => {
               return of(GenericFailedAction({message: "Error while fetching customers"}))
+            })
+          )
+      })
+    )
+  )
+
+  updateCustomerEffect = createEffect(() =>
+    this.actions$.pipe(
+      ofType(UpdateCustomerAction),
+      switchMap(props => {
+        return this.httpClient.put<Boolean>(`${environment.business_base_url}/customers/${props.id}`, props.customer)
+          .pipe(
+            exhaustMap(response => {
+              let resultAction = response ?
+                GenericSuccessAction({message: "Success updated profile information"}) :
+                GenericFailedAction({message: "Failed updating profile information"})
+              return [
+                resultAction,
+                StartFetchCurrentCustomer()
+              ]
+            }),
+            catchError(error => {
+              return of(GenericFailedAction({message: "Failed updating profile information"}));
             })
           )
       })
